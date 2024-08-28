a86bf3e6f761db1a3b97664d7fb274ab69053e35
vane-diarte <vanediarte90@gmail.com>
2024-08-28 18:38:01 -0400

    update function

--- cli.py
+++ cli.py
@@ -3,11 +3,11 @@ from api import obtener_clima
 from utils import formatear_datos
 
 def main():
-    parser = argparse.ArgumentParser(description="Obten los datos del clima con el nombre de la ciudad y el pais.")
+    parser = argparse.ArgumentParser(prog='Clima', description="Obten los datos del clima con el nombre de la ciudad y el pais.")
     
-    parser.add_argument('--city', required=True, help='Nombre de la ciudad')
-    parser.add_argument('--country', required=True, help='Nombre del pais')
-    parser.add_argument('--format', choices=['json', 'csv', 'text'], default='text', help='Formato de salida: json, csv, o text')
+    parser.add_argument('-c', '--ciudad', required=True, help='Nombre de la ciudad')
+    parser.add_argument('-p', '--pais', required=True, help='Nombre del pais')
+    parser.add_argument('-f', '--formato', choices=['json', 'csv', 'text'], default='text', help='Formato de salida: json, csv, o text')
     
     args = parser.parse_args()
     
