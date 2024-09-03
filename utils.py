98132a0995e5852e3e4decea64deab9a5964fa41
vane-diarte <vanediarte90@gmail.com>
2024-09-02 20:25:39 -0400

    update utils.py

--- utils.py
+++ utils.py
@@ -6,22 +6,25 @@ def formatear_datos(obtener_clima, formato_salida):
     if "error" in obtener_clima:
         return obtener_clima["error"]
     
-    main = obtener_clima['main']
-    weather = obtener_clima['weather'][0]
     formatted_data = {
-        "Temperature": main['temp'],
-        "Condition": weather['description'],
+        "City": obtener_clima["ciudad"],
+        "Country": obtener_clima["pais"],
+        "Temperature": obtener_clima["temperatura"],
+        "Condition": obtener_clima["condicion"],
     }
 
-    if formato_salida == 'json':
+    if formato_salida == "json":
         return json.dumps(formatted_data, indent=4)
-    elif formato_salida == 'csv':
+    elif formato_salida == "csv":
         output = StringIO()
         writer = csv.DictWriter(output, fieldnames=formatted_data.keys())
         writer.writeheader()
         writer.writerow(formatted_data)
         return output.getvalue()
-    elif formato_salida == 'text':
-        return f"Temperatura: {formatted_data['Temperature']}°C\nCondicion: {formatted_data['Condition']}"
+    elif formato_salida == "text":
+        return (
+            f"Temperatura: {formatted_data['Temperature']}°C\n"
+            f"Condición: {formatted_data['Condition']}"
+        )
     else:
         return "Formato no compatible"
