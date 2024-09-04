1be9dd0230ce420466ac7ac1423981e86c0091fe
vane-diarte <vanediarte90@gmail.com>
2024-09-04 13:05:11 -0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 pip install -r requirements.txt
 
 # Ejecutar la aplicación con parámetros predeterminados
-python cli.py --ciudad "Asuncion" --pais "Paraguay" --formato "csv"
+python cli.py --ciudad "Asuncion" --pais "Paraguay" --formato "json"
 
 # Ejecutar pruebas automatizadas
 pytest tests/
